1df0754ace570d3611ba306876db01874fc22d1d
Wanderson Leandro de Oliveira <wleandro.oliveira@gmail.com>
2018-05-08 14:47:05 -0300

    Alteração na Classe UsuarioBean

--- src/main/java/br/com/pitagoras/financeiro/web/UsuarioBean.java
+++ src/main/java/br/com/pitagoras/financeiro/web/UsuarioBean.java
@@ -1,5 +1,7 @@
 package br.com.pitagoras.financeiro.web;
 
+import java.util.List;
+
 import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.RequestScoped;
@@ -13,10 +15,18 @@ import br.com.pitagoras.financeiro.usuario.UsuarioRN;
 public class UsuarioBean {
 	private Usuario usuario = new Usuario();
 	private String confirmarSenha;
+	private List<Usuario> lista;
+	private String destinoSalvar;
 
 	public String novo() {
+		this.destinoSalvar = "usuariosucesso";
 		this.usuario = new Usuario();
 		this.usuario.setAtivo(true);
+		return "/publico/usuario";
+	}
+
+	public String editar() {
+		this.confirmarSenha = this.usuario.getSenha();
 		return "/publico/usuario";
 	}
 
@@ -33,7 +43,33 @@ public class UsuarioBean {
 		UsuarioRN usuarioRN = new UsuarioRN();
 		usuarioRN.salvar(this.usuario);
 
-		return "usuariosucesso";
+		return this.destinoSalvar;
+	}
+
+	public String excluir() {
+		UsuarioRN usuarioRN = new UsuarioRN();
+		usuarioRN.excluir(this.usuario);
+		this.lista = null;
+		return null;
+	}
+
+	public String ativar() {
+		if (this.usuario.isAtivo())
+			this.usuario.setAtivo(false);
+		else
+			this.usuario.setAtivo(true);
+
+		UsuarioRN usuarioRN = new UsuarioRN();
+		usuarioRN.salvar(this.usuario);
+		return null;
+	}
+
+	public List<Usuario> getLista() {
+		if (this.lista == null) {
+			UsuarioRN usuarioRN = new UsuarioRN();
+			this.lista = usuarioRN.listar();
+		}
+		return this.lista;
 	}
 
 	public Usuario getUsuario() {
@@ -50,6 +86,14 @@ public class UsuarioBean {
 
 	public void setConfirmarSenha(String confirmarSenha) {
 		this.confirmarSenha = confirmarSenha;
+	}
+
+	public String getDestinoSalvar() {
+		return destinoSalvar;
+	}
+
+	public void setDestinoSalvar(String destinoSalvar) {
+		this.destinoSalvar = destinoSalvar;
 	}
 
 }
